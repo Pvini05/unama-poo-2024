bbd56b02536a5703f54a0a8b8f1535a315aa9044
Pvini05 <pviniciuslazaro@gmail.com>
2024-08-28 12:47:25 +0000

    add: codigos da aula 2

---
+++ BalancoTrimestral.java
@@ -0,0 +1,10 @@
+class BalancoTrimestral{
+    public static void main(String[] args){
+        int gastoJaneiro = 15000;
+        int gastoMarco = 17000;
+        int gastoFevereiro = 23000;
+
+        int gastosTrimestre = gastoJaneiro + gastoFevereiro + gastoMarco;
+        System.out.println(gastosTrimestre);
+    }
+}
---
+++ TesteIdade.java
@@ -0,0 +1,29 @@
+class TesteIdade{
+    public static void main(String[] args){
+    
+        int idade;
+        idade = 21;
+
+        int idadeProximoAno = idade +1;
+        
+        double resultado = idade / 2;
+
+
+        boolean maiorIdade;
+        maiorIdade = idade >= 18;
+
+        boolean tipoSangue = 'o'=='O';
+
+        int i = 5;
+        int j = i;
+        i = i + 1 ;
+
+
+        System.out.println(i);
+        System.out.println(j);
+    }
+}
+
+
+
+
